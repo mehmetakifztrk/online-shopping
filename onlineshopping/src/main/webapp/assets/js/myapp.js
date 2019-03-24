9e2e2af20e6cf15f7347c3e4076d2faa57aea727
mehmetakifztrk <mehmetakiztrk@outlook.com>
2019-03-24 20:52:59 +0300

    bug fix

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -56,16 +56,27 @@ if($table.length){
 				}
 			},
 			{
-				data: 'quantity'
+				data: 'quantity',
+				mRender: function (data) {
+					if (data<1){
+						return '<span style="color:red">Out of Stock!</span>';
+					}
+					return data;
+				}
 			},
 			{
 				data: 'id',
 				bSortable: false,
 				mRender: function(data, type, row){
+					console.log(row);
 					var str = '';
 					str += '<a href="'+window.contextRoot+ '/show/'+data+'/product" class = "btn btn-primary"><span class = "glyphicon glyphicon-eye-open"></span></a> &#160;';
-					str += '<a href="'+window.contextRoot+ '/cart/add/'+data+'/product" class = "btn btn-success"><span class = "glyphicon glyphicon-shopping-cart"></span></a>';
-				
+					
+					if (row.quantity < 1){
+						str += '<a href="javascript:void(0)" class = "btn btn-success disabled"><span class = "glyphicon glyphicon-shopping-cart"></span></a>';
+					} else {
+						str += '<a href="'+window.contextRoot+'/cart/add/'+data+'/product" class = "btn btn-success"><span class = "glyphicon glyphicon-shopping-cart"></span></a>';
+					}
 					return str;
 				}
 			}
